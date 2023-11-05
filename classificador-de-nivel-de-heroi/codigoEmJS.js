6d2f64e7645917cbce21dc9f2e8ed04ea6be7e42
Luiz Marcelo <99672783+luizmipc@users.noreply.github.com>
2023-11-05 12:42:36 -0300

    Update codigoEmJS.js

--- classificador-de-nivel-de-heroi/codigoEmJS.js
+++ classificador-de-nivel-de-heroi/codigoEmJS.js
@@ -8,7 +8,7 @@ class Heroi {
 const niveisValor = [10000, 9000, 8000, 7000, 6000, 5000, 2000, 1000];
 const niveisTexto = ["Radiante", "Imortal", "Ascendente", "Platina", "Ouro", "Ouro-prata", "Prata", "Bronze", "Ferro"];
 // Heroi de exemplo
-let heroi = new Heroi("Luiz Marcelo", 10001, "");
+let heroi = new Heroi("Luiz Marcelo", 10001);
 
 if (heroi.experiencia <= niveisValor[7]) {
     console.log(`O herói de nome ${heroi.nome} está no nível de ${niveisTexto[8]}.`);
