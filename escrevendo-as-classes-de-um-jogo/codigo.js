2731b05a198c82f57ff78c3a4ba85534b3df0c34
Luiz Marcelo <99672783+luizmipc@users.noreply.github.com>
2023-11-12 02:00:12 -0300

    Update codigo.js

--- escrevendo-as-classes-de-um-jogo/codigo.js
+++ escrevendo-as-classes-de-um-jogo/codigo.js
@@ -2,14 +2,14 @@ const tipos = ["mago", "guerreiro", "monge", "ninja"];
 const ataques = ["magia", "espada", "artes marciais", "shuriken"];
 
 class Heroi {
-    constructor(nome, idade, tipo) {
+    constructor ( nome, idade, tipo ) {
         this.nome=nome;
         this.idade=idade;
         this.tipo=tipo;
     }
     atacar() {
-        for (let i = 0; i < 4; i++) {
-            if (this.tipo == tipos[i]) {
+        for ( let i = 0; i < tipos.length; i++ ) {
+            if ( this.tipo == tipos[i] ) {
                 console.log(`O ${this.tipo} atacou usando ${ataques[i]}.`);
                 break;
             }
@@ -18,5 +18,5 @@ class Heroi {
 }
 
 // caso de exemplo
-let heroi = new Heroi("Luiz Marcelo", 21, tipos[0]);
+let heroi = new Heroi ( "Luiz Marcelo", 21, tipos[3] );
 heroi.atacar();
