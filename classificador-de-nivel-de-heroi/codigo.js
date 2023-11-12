24c904dbd17f32e4e1f128de4784451f02f844ac
Luiz Marcelo <99672783+luizmipc@users.noreply.github.com>
2023-11-12 01:54:55 -0300

    Update codigo.js

--- classificador-de-nivel-de-heroi/codigo.js
+++ classificador-de-nivel-de-heroi/codigo.js
@@ -8,7 +8,7 @@ class Heroi {
 const nivelValor = [10000, 9000, 8000, 7000, 5000, 2000, 1000];
 const nivelTexto = ["Radiante", "Imortal", "Ascendente", "Platina", "Ouro", "Prata", "Bronze", "Ferro"];
 
-let heroi = new Heroi( "Luiz Marcelo", 1001 );
+let heroi = new Heroi( "Luiz Marcelo", 10001 );
 
 if ( heroi.experiencia <= nivelValor[nivelValor.length-1 ]) {
     console.log(`O herói de nome ${heroi.nome} está no nível de ${nivelTexto[nivelTexto.length-1]}.`);
